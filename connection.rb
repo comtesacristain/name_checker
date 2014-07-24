b9c3c8f9ba79e50294896da392fabb1764b078ec
Michael Sexton <mick.sexton@gmail.com>
2014-07-24 13:22:49 +1000

    Changed permissions

--- connection.rb
+++ connection.rb
@@ -1,5 +1,5 @@
 dbconfig = YAML::load(File.open('database.yml'))
 
-ActiveRecord::Base.establish_connection(dbconfig["test"])
+ActiveRecord::Base.establish_connection(dbconfig["production"])
 
 ActiveRecord::Base.connection.execute("ALTER SESSION set NLS_DATE_FORMAT ='DD-MON-FXYYYY'")
