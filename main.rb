6bf4db849a8c626e50b95c9c0e71739ecdcf3a1a
Michael Sexton <mick.sexton@gmail.com>
2014-07-24 16:21:24 +1000

    Added question for duplicates

--- main.rb
+++ main.rb
@@ -13,10 +13,11 @@ require File.join('./lib', 'website.rb')
 websites = Website.all
 
 websites.each do |website|
-  puts "Searching for "
+  puts "Searching for #{website.description} with URL #{website.url} (websiteno: #{website.websiteno})"
   name_part = website.description.split(/ /).first.gsub(/'/,"\\'")
   found_websites = Website.where("upper(description) like '%#{name_part.upcase}%'")
-  found_websites.each_with_index do |fw|
-    puts "#{fw.description} #{fw.url}"
+  found_websites.each_with_index do |fw, i|
+    puts "Is #{fw.description} with URL #{fw.url} (websiteno: #{website.websiteno}) a duplicate? (y/n)"
+    answer=gets.chomp
   end
 end
