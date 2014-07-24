ab8a3e7de19dc5efc834d69f5fad89824212ade1
Michael Sexton <mick.sexton@gmail.com>
2014-07-24 16:32:09 +1000

    Added clean_deleted function and moved queries into Website model

--- main.rb
+++ main.rb
@@ -15,9 +15,20 @@ websites = Website.all
 websites.each do |website|
   puts "Searching for #{website.description} with URL #{website.url} (websiteno: #{website.websiteno})"
   name_part = website.description.split(/ /).first.gsub(/'/,"\\'")
-  found_websites = Website.where("upper(description) like '%#{name_part.upcase}%'")
+  found_websites = Website.by_description(name_part)
   found_websites.each_with_index do |fw, i|
     puts "Is #{fw.description} with URL #{fw.url} (websiteno: #{website.websiteno}) a duplicate? (y/n)"
     answer=gets.chomp
   end
 end
+
+def clean_deleted
+  deleted_websites  = Website.to_delete
+  deleted_websites.each do |dw|
+    dw.delete
+  end
+end
+
+# Codes for website clean up
+# DELETE:DUPLICATE
+# DELETE:BROKEN
